3ed012737a05538c04704b1440cc44669cf886c6
Rory Dungan <rorydungan@gmail.com>
2019-10-02 22:38:11 +1000

    Create 16 buttons, play around with styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react'
 import './App.css'
 import SoundButton from './SoundButton'
+import '@blueprintjs/core/lib/css/blueprint.css'
+import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 
 function App () {
   return (
     <div className="App">
-      <header className="App-header">
+      <div style={{ display: 'flex' }}>
         <SoundButton src='Cowbell.wav'/>
-      </header>
+        <SoundButton src='Cowbell.wav'/>
+        <SoundButton src='Cowbell.wav'/>
+        <SoundButton src='Cowbell.wav'/>
+      </div>
+      <div style={{ display: 'flex' }}>
+        <SoundButton src='Cowbell.wav'/>
+        <SoundButton src='Cowbell.wav'/>
+        <SoundButton src='Cowbell.wav'/>
+        <SoundButton src='Cowbell.wav'/>
+      </div>
+      <div style={{ display: 'flex' }}>
+        <SoundButton src='Cowbell.wav'/>
+        <SoundButton src='Cowbell.wav'/>
+        <SoundButton src='Cowbell.wav'/>
+        <SoundButton src='Cowbell.wav'/>
+      </div>
+      <div style={{ display: 'flex' }}>
+        <SoundButton src='Cowbell.wav'/>
+        <SoundButton src='Cowbell.wav'/>
+        <SoundButton src='Cowbell.wav'/>
+        <SoundButton src='Cowbell.wav'/>
+      </div>
     </div>
   )
 }
--- src/SoundButton.js
+++ src/SoundButton.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default class SoundButton extends React.Component {
   render () {
-    return <div>
+    return <div
+      className='bp3-card bp3-elevation-1 bp3-interactive'
+      style={{ flex: '1 1 auto', margin: '10px' }}
+      onClick={ () => {
+        console.log('click!')
+        this.audioElement.play()
+      }} >
       <audio ref={element => { this.audioElement = element }}>
         <source src={ this.props.src }/>
       </audio>
-      <button onClick={ () => {
-        console.log('click!')
-        this.audioElement.play()
-      }}>
-        hello
-      </button>
     </div>
   }
 }
