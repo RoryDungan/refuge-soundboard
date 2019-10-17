c0c7ffc5b237402bbfadc2c309d8db1ca90db3bd
Rory Dungan <rorydungan@gmail.com>
2019-10-17 23:53:40 +1100

    Updates

---
+++ precache-manifest.753fd23d31a3ef73cd30ebdcd2493579.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e0ec06ac1c43cdd38f201b352a2764",
+    "url": "/refuge-soundboard/index.html"
+  },
+  {
+    "revision": "f538c3b0ecb02d9e54b0",
+    "url": "/refuge-soundboard/static/css/2.990444bb.chunk.css"
+  },
+  {
+    "revision": "3e1724070fb8fed91b25",
+    "url": "/refuge-soundboard/static/css/main.649456db.chunk.css"
+  },
+  {
+    "revision": "f538c3b0ecb02d9e54b0",
+    "url": "/refuge-soundboard/static/js/2.0797bc12.chunk.js"
+  },
+  {
+    "revision": "3e1724070fb8fed91b25",
+    "url": "/refuge-soundboard/static/js/main.e3f9a8e0.chunk.js"
+  },
+  {
+    "revision": "e64236c9e1cb87c3247e",
+    "url": "/refuge-soundboard/static/js/runtime~main.a9a65a14.js"
+  },
+  {
+    "revision": "5fa1b8f25b4aa8787f70d5b6b7f0b90f",
+    "url": "/refuge-soundboard/static/media/icons-16.5fa1b8f2.ttf"
+  },
+  {
+    "revision": "6fdb722c94434811f89a68eafc4531f8",
+    "url": "/refuge-soundboard/static/media/icons-16.6fdb722c.eot"
+  },
+  {
+    "revision": "cd1a26696ebf17a89545a3f9067d7028",
+    "url": "/refuge-soundboard/static/media/icons-16.cd1a2669.woff"
+  },
+  {
+    "revision": "14279fa7b78ac2230a015925b603a1f7",
+    "url": "/refuge-soundboard/static/media/icons-20.14279fa7.eot"
+  },
+  {
+    "revision": "657723dc996cf0ae6b4c6110f5d4ddab",
+    "url": "/refuge-soundboard/static/media/icons-20.657723dc.woff"
+  },
+  {
+    "revision": "90247cfdde1bd7b76d804a797d5dec56",
+    "url": "/refuge-soundboard/static/media/icons-20.90247cfd.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.071b4e593b4c523eb66f49d35fc07cbe.js"
+  "/refuge-soundboard/precache-manifest.753fd23d31a3ef73cd30ebdcd2493579.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/refuge-soundboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
