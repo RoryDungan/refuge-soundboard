5be6bccaf7907060027b3bf432fe5fc273ed2248
Rory Dungan <rorydungan@gmail.com>
2019-10-17 22:11:58 +1100

    Lighten button backgrounds slightly

--- src/SoundButton.js
+++ src/SoundButton.js
@@ -18,21 +18,33 @@ const getId = () => id++
 export default class SoundButton extends React.Component {
   render () {
     const id = getId()
+    const defaultColour = '#394b59'
 
     return <div
-      className='SoundButton bp3-card bp3-elevation-2 bp3-interactive'
-      style={
-        this.props.state === ButtonStates.playing
-          ? { animation: `SoundButton_pulse_${id} 1s ease-out 0s alternate infinite` }
-          : {}
+      className={
+        'SoundButton bp3-card bp3-elevation-2 bp3-interactive ' +
+          (this.props.state === ButtonStates.playing ? `SoundButton_playing_${id}` : `SoundButton_notplaying_${id}`)
       }
       onClick={() => {
         this.props.play()
       }} >
       <style>{`
+        .SoundButton_notplaying_${id} {
+          background-color: ${defaultColour} !important;
+          transition-property: transform, box-shadow, background-color !important;
+          transition-duration: 0.3s;
+          transition-easing: ease-in-out;
+        }
+
+        .SoundButton_playing_${id} {
+          animation: SoundButton_pulse_${id} 1s ease-out 0s alternate infinite;
+          transition-property: transform, box-shadow, background-color !important;
+          transition-duration: 0.5s;
+          transition-easing: ease-in-out;
+        }
         @keyframes SoundButton_pulse_${id} {
           0% {
-            background-color: rgb(48, 64, 77);
+            background-color: ${defaultColour};
           }
           100% {
             background-color: ${this.props.colour || 'red'}
